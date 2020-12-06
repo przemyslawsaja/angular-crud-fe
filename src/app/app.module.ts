0aade6472cc3adbfbfd8f4c3f732688ed12782e8
unknown <przemyslawsaja@gmail.com>
2020-12-06 09:32:43 +0100

    material modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemsListComponent } from './components/items-list/items-list.component'
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from  '@angular/material/card';
+import { MatIconModule } from  '@angular/material/icon';
+import { MatInputModule } from  '@angular/material/input';
+import { MatListModule } from  '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
